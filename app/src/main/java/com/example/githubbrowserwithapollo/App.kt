4add10a85959d160e54b9bba1314e2a0e287b5c2
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-06-27 10:37:44 +0900

    setup koin

--- app/src/main/java/com/example/githubbrowserwithapollo/App.kt
+++ app/src/main/java/com/example/githubbrowserwithapollo/App.kt
@@ -1,13 +1,21 @@
 package com.example.githubbrowserwithapollo
 
 import android.app.Application
+import com.example.githubbrowserwithapollo.di.apiModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
+        super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(apiModule)
+        }
     }
 }
---
+++ app/src/main/java/com/example/githubbrowserwithapollo/di/ApiModule.kt
@@ -0,0 +1,23 @@
+package com.example.githubbrowserwithapollo.di
+
+import com.apollographql.apollo.ApolloClient
+import com.example.githubbrowserwithapollo.BuildConfig
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
+
+val apiModule = module {
+    single {
+        val okHttpClient = OkHttpClient().newBuilder()
+            .authenticator { _, response ->
+                response.request()
+                    .newBuilder()
+                    .addHeader("Authorization", "Bearer ${BuildConfig.GITHUB_ACCESS_TOKEN}")
+                    .build()
+            }.build()
+
+        ApolloClient.builder()
+            .serverUrl("https://api.github.com/graphql")
+            .okHttpClient(okHttpClient)
+            .build()
+    }
+}
