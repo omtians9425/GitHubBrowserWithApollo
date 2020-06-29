57e457d6a7f768da144b069cf5861fd6ade3f211
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-06-29 21:36:30 +0900

    add Epoxy

--- app/src/main/java/com/example/githubbrowserwithapollo/main/MainViewModel.kt
+++ app/src/main/java/com/example/githubbrowserwithapollo/main/MainViewModel.kt
@@ -6,7 +6,7 @@ import com.example.githubbrowserwithapollo.data.GitHubRepoRepository
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
-class MainViewModel(private val gitHubRepoRepository: GitHubRepoRepository) : ViewModel() {
+class MainViewModel(gitHubRepoRepository: GitHubRepoRepository) : ViewModel() {
     private val _myRepos = MutableLiveData<MyReposQuery.Data?>()
     val myRepos: LiveData<MyReposQuery.Data?>
         get() = _myRepos.distinctUntilChanged()
