c092e36f05a595f9c5aa072b80e55ad167d39bf2
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-06-29 21:26:57 +0900

    implement MainViewModel

--- app/src/main/java/com/example/githubbrowserwithapollo/main/MainFragment.kt
+++ app/src/main/java/com/example/githubbrowserwithapollo/main/MainFragment.kt
@@ -5,17 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.coroutines.toFlow
-import com.example.githubbrowserwithapollo.BuildConfig
-import com.example.githubbrowserwithapollo.MyReposQuery
+import androidx.lifecycle.Observer
 import com.example.githubbrowserwithapollo.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -29,7 +21,6 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Timber.d("onCreateView() called with: inflater = [$inflater], container = [$container], savedInstanceState = [$savedInstanceState]")
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
@@ -37,26 +28,8 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         Timber.i("$mainViewModel")
 
-        val okHttpClient = OkHttpClient().newBuilder()
-            .authenticator { _, response ->
-                response.request()
-                    .newBuilder()
-                    .addHeader("Authorization", "Bearer ${BuildConfig.GITHUB_ACCESS_TOKEN}")
-                    .build()
-            }.build()
-
-        val apolloClient = ApolloClient.builder()
-            .serverUrl("https://api.github.com/graphql")
-            .okHttpClient(okHttpClient)
-            .build()
-
-        lifecycleScope.launch {
-            val flow = apolloClient.query(
-                MyReposQuery(
-                    first = 4
-                )
-            ).toFlow().map { it.data }
-            flow.collect { Timber.i("$it") }
-        }
+        mainViewModel.myRepos.observe(viewLifecycleOwner, Observer {
+            Timber.i("$it")
+        })
     }
 }
--- app/src/main/java/com/example/githubbrowserwithapollo/main/MainViewModel.kt
+++ app/src/main/java/com/example/githubbrowserwithapollo/main/MainViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.githubbrowserwithapollo.main
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.githubbrowserwithapollo.MyReposQuery
 import com.example.githubbrowserwithapollo.data.GitHubRepoRepository
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class MainViewModel(private val gitHubRepoRepository: GitHubRepoRepository) : ViewModel() {
+    private val _myRepos = MutableLiveData<MyReposQuery.Data?>()
+    val myRepos: LiveData<MyReposQuery.Data?>
+        get() = _myRepos.distinctUntilChanged()
+
+    init {
+        gitHubRepoRepository.fetchMyRepository().onEach {
+            _myRepos.value = it
+        }.launchIn(viewModelScope)
+    }
 }
