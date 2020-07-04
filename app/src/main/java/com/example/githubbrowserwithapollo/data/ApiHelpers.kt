bdaec3214da5d224b874cffeff74c1f7bdf66b44
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-07-05 03:20:59 +0900

    add ApiHelpersTest

--- app/src/main/java/com/example/githubbrowserwithapollo/data/ApiHelpers.kt
+++ app/src/main/java/com/example/githubbrowserwithapollo/data/ApiHelpers.kt
@@ -1,18 +1,36 @@
 package com.example.githubbrowserwithapollo.data
 
 import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.coroutines.toFlow
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import timber.log.Timber
 
 @ExperimentalCoroutinesApi
 fun <T> ApolloCall<T>.toLceFlow() = toFlow().map { response ->
-    response.data?.let { Lce.Content<T>(it) }
-        ?: run {
-            val msg = response.errors!!.joinToString()
-            Timber.e(msg)
-            Lce.Error<T>(msg)
-        }
-}.onStart { emit(Lce.Loading<T>()) }
+    response.data?.let {
+        Lce.Content<T>(it)
+    } ?: run {
+        val msg = response.errors!!.joinToString()
+        Timber.e(msg)
+        Lce.Error<T>(msg)
+    }
+}.onStart {
+    emit(Lce.Loading<T>())
+}
+
+// non-extension version (easy to test)
+fun <T> toLceFlow(flow: Flow<Response<T>>) = flow.map { response ->
+    response.data?.let {
+        Lce.Content<T>(it)
+    } ?: run {
+        val msg = response.errors!!.joinToString()
+        Timber.e(msg)
+        Lce.Error<T>(msg)
+    }
+}.onStart {
+    emit(Lce.Loading<T>())
+}
--- app/src/test/java/com/example/githubbrowserwithapollo/data/ApiHelpersTest.kt
+++ app/src/test/java/com/example/githubbrowserwithapollo/data/ApiHelpersTest.kt
@@ -1,24 +1,73 @@
 package com.example.githubbrowserwithapollo.data
 
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.coroutines.toFlow
 import com.example.githubbrowserwithapollo.MainCoroutineRule
+import com.example.githubbrowserwithapollo.MyReposQuery
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Rule
+import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class ApiHelpersTest {
 
     @get:Rule
     var coroutineRule = MainCoroutineRule()
 
-//    @Test
-//    fun toLceFlow() = coroutineRule.runBlockingTest {
-//        val response = mockk<Response<MyReposQuery.Data>>(relaxed = true)
-//        val data = MyReposQuery.Data(mockk(relaxed = true))
-//        every { response.data.hint(MyReposQuery.Data::class) } returns data
-//
-//        val apolloCall = mockk<ApolloClient>(relaxed = true)
-//        every { apolloCall.query(any<MyReposQuery>()).toFlow(). } returns flowOf(response)
-//
-//        val result = apolloCall.query(MyReposQuery(first = 1)).toLceFlow().collect {
-//            assertThat(it).isEqualTo(data)
-//        }
-//    }
+    private val response: Response<MyReposQuery.Data> = mockk(relaxed = true)
+    private val apolloCall: ApolloClient = mockk(relaxed = true)
+
+    private fun mockApolloCallToFlow() {
+        // use mockkStatic with package name and file name in order to mock the extension function.
+        mockkStatic("com.apollographql.apollo.coroutines.CoroutinesExtensionsKt")
+        every { apolloCall.query(any<MyReposQuery>()).toFlow() } returns flowOf(response)
+    }
+
+    @Test
+    fun `toLceFlow is success when data is non-null`() = coroutineRule.runBlockingTest {
+        every { response.data } returns mockk(relaxed = true)
+
+        mockApolloCallToFlow()
+
+        val result = apolloCall.query(MyReposQuery(1)).toLceFlow().toList()
+        assertThat(result[0]).isInstanceOf(Lce.Loading::class.java)
+        assertThat(result[1]).isInstanceOf(Lce.Content::class.java)
+    }
+
+    @Test
+    fun `toLceFlow is error when data is null`() = coroutineRule.runBlockingTest {
+        every { response.data } returns null
+
+        mockApolloCallToFlow()
+
+        val result = apolloCall.query(MyReposQuery(1)).toLceFlow().toList()
+        assertThat(result[0]).isInstanceOf(Lce.Loading::class.java)
+        assertThat(result[1]).isInstanceOf(Lce.Error::class.java)
+    }
+
+    @Test
+    fun `toLceFlow non-ext success when data is non-null`() = coroutineRule.runBlockingTest {
+        every { response.data } returns mockk()
+
+        val result = toLceFlow(flowOf(response)).toList()
+        assertThat(result[0]).isInstanceOf(Lce.Loading::class.java)
+        assertThat(result[1]).isInstanceOf(Lce.Content::class.java)
+    }
+
+    @Test
+    fun `toLceFlow non-ext error when data is null`() = coroutineRule.runBlockingTest {
+        every { response.data } returns null
+
+        val result = toLceFlow(flowOf(response)).toList()
+        assertThat(result[0]).isInstanceOf(Lce.Loading::class.java)
+        assertThat(result[1]).isInstanceOf(Lce.Error::class.java)
+    }
 }
